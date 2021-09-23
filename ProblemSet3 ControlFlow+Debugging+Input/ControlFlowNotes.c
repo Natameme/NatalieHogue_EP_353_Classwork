9a485bbe774ab16632071d8d2b316488b0db884c
Natameme <60371038+Natameme@users.noreply.github.com>
2021-09-23 14:26:35 -0400

    added problemset 3 folder

--- ProblemSet3 ControlFlow+Debugging+Input/ControlFlowNotes.c
+++ ProblemSet3 ControlFlow+Debugging+Input/ControlFlowNotes.c
@@ -2,6 +2,23 @@
 //CONTROL FLOW
 int main(){
 
+  char midiNote = 127;
+  // Print a message, if MIDI note is is out of range
+  if(midiNote < 0 || midiNote > 127)
+    printf("\nThe note value entered is Invalid.\n");
 
-return 0;  
+  //---------------------------------------------
+  // 2. if-then statement with block
+  // if (/* condition */) {
+  //   /* execute this code in the block */
+  // }
+
+  // Print a message, if MIDI note is is between 0 and 127
+  if(midiNote >= 0 && midiNote <= 127) {
+    printf("The note value entered is valid.\n");
+    printf("MIDI note value is %d.\n", midiNote);
+  }
+
+
+return 0;
 }
