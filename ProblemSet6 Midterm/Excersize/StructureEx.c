ed78a1ed0e0b9a16a25ae660be2b9a9aeb131047
Natameme <60371038+Natameme@users.noreply.github.com>
2021-10-14 15:40:56 -0400

    added week 6 notes

---
+++ ProblemSet6 Midterm/Excersize/StructureEx.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct point
+{
+  int x;
+  int y;
+} point;
+
+typedef struct size
+{
+  int width;
+  int height;
+} size;
+
+typedef struct rectangle
+{
+  point.x = 1;
+  point.y = 4;
+  size.width = 40;
+  size.height = 20;
+} rect;
+
+int main(){
+
+  printf("The base of the rectangle is at x: %i, y: %i with dimensions w: %i, h: %i", rect.point.x, rect.point.y, rect.size.width, rect.size.height);
+
+  return 0;
+}
