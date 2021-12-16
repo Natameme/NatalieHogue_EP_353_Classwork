0957517a2437d115e36ef606631b443c18af9617
Natameme <60371038+Natameme@users.noreply.github.com>
2021-12-16 00:06:31 -0500

    final edits

--- Final/Source/StMaximiliansOrganMKII.c
+++ Final/Source/StMaximiliansOrganMKII.c
@@ -38,19 +38,19 @@
 //-------------------------------------------------------------------
 //PER STOP ARRAY DEFINITIONS
 //for use in process() and loadStops()
-float bourEight[256], //bourdon 8
-      bourFour[256],  //bourdon 4
-      clarFour[256],  //clarion 4
-      fifTwo[256],    //fifteenth 2
-      melEight[256],  //melodia 8
-      mixOne[256],    //mixture 1
-      octFour[256],   //octave 4
-      prinEight[256], //principle 8
-      truEight[256],  //trumpet 8
-      truSixt[256],   //trumpet 16
-      tweTwo[256],    //twelfth 2 2/3
-      vioEight[256],  //violone 8
-      test[256];      //test oscillator for debugging
+float bourEight[441], //bourdon 8
+      bourFour[441],  //bourdon 4
+      clarFour[441],  //clarion 4
+      fifTwo[441],    //fifteenth 2
+      melEight[441],  //melodia 8
+      mixOne[441],    //mixture 1
+      octFour[441],   //octave 4
+      prinEight[441], //principle 8
+      truEight[441],  //trumpet 8
+      truSixt[441],   //trumpet 16
+      tweTwo[441],    //twelfth 2 2/3
+      vioEight[441],  //violone 8
+      test[441];      //test oscillator for debugging
 
 //NOTE Spreadsheets containing stop data can be found in Final/deprecated/stops
 //single cycle .wav files can additionally be found in Final/deprecated/waves
@@ -205,11 +205,21 @@ void process(float *buffer, unsigned long numFrames, void *userData){
   SineWave *sineWave = (SineWave *) userData;
   float theta = 0, sine = 0, voice = 0; // theta: angular velocity; sine: sample value; voice: phase per voice;
   int   val[128]; //array of MIDI CC values
-  static float vol[128]; //mirrors val with float values between 0 and 1
-  vol[7] = 1.0f;
-  for(int i = 12; i <= 23; i++){
-    vol[i] = 1.0f;
-  }
+  float vol[128]; //mirrors val with float values between 0 and 1
+  //vol[7] = 1.0f;
+  //vol[12] = 1.0f;
+  //vol[13] = 1.0f;
+  //vol[14] = 1.0f;
+  //vol[15] = 1.0f;
+  //vol[16] = 1.0f;
+  //vol[17] = 1.0f;
+  //vol[18] = 1.0f;
+  //vol[19] = 1.0f;
+  //vol[20] = 1.0f;
+  //vol[21] = 1.0f;
+  //vol[22] = 1.0f;
+  //vol[23] = 1.0f;
+
 
   //load stops
   loadStops();
@@ -222,7 +232,7 @@ void process(float *buffer, unsigned long numFrames, void *userData){
       for(unsigned long n = 0; n < numFrames; n += kNumChannels){
 
     //convert sineWave->phase[q] into an integer to drive arrays
-    voice = sineWave->phase[q] * (256); //voice tracks phase per voice
+    voice = sineWave->phase[q] * (441); //voice tracks phase per voice
     int dex = voice; //dex drives arrays
     //printf("%f, %i\n", voice, dex);//debugging
 
@@ -239,6 +249,7 @@ void process(float *buffer, unsigned long numFrames, void *userData){
     float tSixt  = truSixt[dex]   * (vol[7]) * (vol[21]);
     float tTwo   = tweTwo[dex]    * (vol[7]) * (vol[22]);
     float vEight = vioEight[dex]  * (vol[7]) * (vol[23]);
+
     float te     = test[dex] * vol[7];
 
     //add stops together
@@ -376,9 +387,9 @@ void process(float *buffer, unsigned long numFrames, void *userData){
 void loadStops(){
   //STOP DEFINITIONS FOR USE IN loadStops()
 
-  for(int i = 0; i <= 256; i++){
+  for(int i = 0; i <= 441; i++){
     float dex = i; //convert i to floating point
-    float phasor = (dex/(256/2)) - 1.0f; //convert floating poin index to value 0 ~ 1
+    float phasor = (dex/(441/2)) - 1.0f; //convert floating poin index to value 0 ~ 1
 
     //////////////////
     //STOP DEFINITIONS
@@ -392,8 +403,7 @@ void loadStops(){
                                (sin(phasor *  M_PI * 2.0f * 3.0f * 4.0f) * 0.00944f)+
                                (sin(phasor *  M_PI * 2.0f * 5.0f * 4.0f) * 0.00177f);
                    //Clarion Four
-                clarFour[i] = (0.5 *
-                               (sin(phasor *  M_PI * 2.0f * 1.0f  * 4.0f ) * 0.0901f)+
+                clarFour[i] =  (sin(phasor *  M_PI * 2.0f * 1.0f  * 4.0f ) * 0.0901f)+
                                (sin(phasor *  M_PI * 2.0f * 2.0f  * 4.0f ) * 0.108f)+
                                (sin(phasor *  M_PI * 2.0f * 3.0f  * 4.0f ) * 0.0609f)+
                                (sin(phasor *  M_PI * 2.0f * 4.0f  * 4.0f ) * 0.0319f)+
@@ -407,10 +417,9 @@ void loadStops(){
                                (sin(phasor *  M_PI * 2.0f * 12.0f * 4.0f ) * 0.0151f)+
                                (sin(phasor *  M_PI * 2.0f * 13.0f * 4.0f ) * 0.0123f)+
                                (sin(phasor *  M_PI * 2.0f * 15.0f * 4.0f ) * 0.00237f)+
-                               (sin(phasor *  M_PI * 2.0f * 16.0f * 4.0f ) * 0.00139f));
+                               (sin(phasor *  M_PI * 2.0f * 16.0f * 4.0f ) * 0.00139f);
                      //Fifteenth two
-                  fifTwo[i] = (0.5 *
-                              (sin(phasor *  M_PI * 2.0f * 1.0f   * 8.0f ) * 0.116f)+
+                  fifTwo[i] = (sin(phasor *  M_PI * 2.0f * 1.0f   * 8.0f ) * 0.116f)+
                               (sin(phasor *  M_PI * 2.0f * 2.0f   * 8.0f ) * 0.0223f)+
                               (sin(phasor *  M_PI * 2.0f * 3.0f   * 8.0f ) * 0.329f)+
                               (sin(phasor *  M_PI * 2.0f * 4.0f   * 8.0f ) * 0.00371f)+
@@ -421,10 +430,9 @@ void loadStops(){
                               (sin(phasor *  M_PI * 2.0f * 9.0f   * 8.0f ) * 0.000350f)+
                               (sin(phasor *  M_PI * 2.0f * 10.0f  * 8.0f ) * 0.000881f)+
                               (sin(phasor *  M_PI * 2.0f * 11.227 * 8.0f ) * 0.000489f)+
-                              (sin(phasor *  M_PI * 2.0f * 12.0f  * 8.0f ) * 0.000323f));
+                              (sin(phasor *  M_PI * 2.0f * 12.0f  * 8.0f ) * 0.000323f);
                      //melodia Eight
-                  melEight[i] = (0.5 *
-                                (sin(phasor *  M_PI * 2.0f * 1.0f * 2.0f ) * 0.331f)+
+                  melEight[i] = (sin(phasor *  M_PI * 2.0f * 1.0f * 2.0f ) * 0.331f)+
                                 (sin(phasor *  M_PI * 2.0f * 2.0f * 2.0f ) * 0.0346f)+
                                 (sin(phasor *  M_PI * 2.0f * 3.0f * 2.0f ) * 0.0518f)+
                                 (sin(phasor *  M_PI * 2.0f * 4.0f * 2.0f ) * 0.00803f)+
@@ -432,10 +440,9 @@ void loadStops(){
                                 (sin(phasor *  M_PI * 2.0f * 6.0f * 2.0f ) * 0.00254f)+
                                 (sin(phasor *  M_PI * 2.0f * 7.0f * 2.0f ) * 0.00156f)+
                                 (sin(phasor *  M_PI * 2.0f * 8.0f * 2.0f ) * 0.00149f)+
-                                (sin(phasor *  M_PI * 2.0f * 10.0f * 2.0f) * 0.00102f));
+                                (sin(phasor *  M_PI * 2.0f * 10.0f * 2.0f) * 0.00102f);
                      //Mixture 1 1/3
-                  mixOne[i] = (0.5 *
-                              (sin(phasor *  M_PI * 2.0f * 1.0f    * 16.0f ) * 0.0421f)+
+                  mixOne[i] = (sin(phasor *  M_PI * 2.0f * 1.0f    * 16.0f ) * 0.0421f)+
                               (sin(phasor *  M_PI * 2.0f * 1.331f  * 16.0f ) * 0.0588f)+
                               (sin(phasor *  M_PI * 2.0f * 2.0f    * 16.0f ) * 0.0623f)+
                               (sin(phasor *  M_PI * 2.0f * 2.662f  * 16.0f ) * 0.0117f)+
@@ -458,10 +465,9 @@ void loadStops(){
                               (sin(phasor *  M_PI * 2.0f * 18.0f   * 16.0f ) * 0.000254f)+
                               (sin(phasor *  M_PI * 2.0f * 18.641f * 16.0f ) * 0.000147f)+
                               (sin(phasor *  M_PI * 2.0f * 21.304f * 16.0f ) * 0.000285f)+
-                              (sin(phasor *  M_PI * 2.0f * 23.967f * 16.0f ) * 0.000114f));
+                              (sin(phasor *  M_PI * 2.0f * 23.967f * 16.0f ) * 0.000114f);
                      //Octave Four
-                  octFour[i] = (0.5 *
-                               (sin(phasor *  M_PI * 2.0f * 1.0f    * 4.0f )  * 0.221f)+
+                  octFour[i] = (sin(phasor *  M_PI * 2.0f * 1.0f    * 4.0f )  * 0.221f)+
                                (sin(phasor *  M_PI * 2.0f * 2.0f    * 4.0f )  * 0.0749f)+
                                (sin(phasor *  M_PI * 2.0f * 3.0f    * 4.0f )  * 0.0165f)+
                                (sin(phasor *  M_PI * 2.0f * 4.0f    * 4.0f )  * 0.0113f)+
@@ -476,10 +482,9 @@ void loadStops(){
                                (sin(phasor *  M_PI * 2.0f * 13.0f   * 4.0f )  * 0.000412f)+
                                (sin(phasor *  M_PI * 2.0f * 14.0f   * 4.0f )  * 0.000371f)+
                                (sin(phasor *  M_PI * 2.0f * 15.0f   * 4.0f )  * 0.000295f)+
-                               (sin(phasor *  M_PI * 2.0f * 16.281f * 4.0f )  * 0.000237f));
+                               (sin(phasor *  M_PI * 2.0f * 16.281f * 4.0f )  * 0.000237f);
                      //Principle Eight
-                  prinEight[i] = (0.5 *
-                                 (sin(phasor *  M_PI * 2.0f * 1.0f  * 2.0f ) * 0.167f)+
+                  prinEight[i] = (sin(phasor *  M_PI * 2.0f * 1.0f  * 2.0f ) * 0.167f)+
                                  (sin(phasor *  M_PI * 2.0f * 2.0f  * 2.0f ) * 0.0785f)+
                                  (sin(phasor *  M_PI * 2.0f * 3.0f  * 2.0f ) * 0.0251f)+
                                  (sin(phasor *  M_PI * 2.0f * 4.0f  * 2.0f ) * 0.0384f)+
@@ -490,10 +495,9 @@ void loadStops(){
                                  (sin(phasor *  M_PI * 2.0f * 9.0f  * 2.0f ) * 0.00146f)+
                                  (sin(phasor *  M_PI * 2.0f * 10.0f * 2.0f ) * 0.00136f)+
                                  (sin(phasor *  M_PI * 2.0f * 11.0f * 2.0f ) * 0.00109f)+
-                                 (sin(phasor *  M_PI * 2.0f * 18.0f * 2.0f ) * 0.00108f));
+                                 (sin(phasor *  M_PI * 2.0f * 18.0f * 2.0f ) * 0.00108f);
                      //trumpet 8
-                  truEight[i] = (0.5 *
-                                (sin(phasor *  M_PI * 2.0f * 1.0f  * 2.0f ) * 0.0346f)+
+                  truEight[i] = (sin(phasor *  M_PI * 2.0f * 1.0f  * 2.0f ) * 0.0346f)+
                                 (sin(phasor *  M_PI * 2.0f * 2.0f  * 2.0f ) * 0.0371f)+
                                 (sin(phasor *  M_PI * 2.0f * 3.0f  * 2.0f ) * 0.00794f)+
                                 (sin(phasor *  M_PI * 2.0f * 4.0f  * 2.0f ) * 0.0582f)+
@@ -513,11 +517,9 @@ void loadStops(){
                                 (sin(phasor *  M_PI * 2.0f * 18.0f * 2.0f ) * 0.00269f)+
                                 (sin(phasor *  M_PI * 2.0f * 19.0f * 2.0f ) * 0.00231f)+
                                 (sin(phasor *  M_PI * 2.0f * 20.0f * 2.0f ) * 0.00121f)+
-                                (sin(phasor *  M_PI * 2.0f * 21.0f * 2.0f ) * 0.000363f)
-                                );
+                                (sin(phasor *  M_PI * 2.0f * 21.0f * 2.0f ) * 0.000363f);
                                 //trumpet 16
-                  truSixt[i] =  (0.5 *
-                                (sin(phasor *  M_PI * 2.0f * (1.0f)  ) * 0.0346f)+
+                  truSixt[i] =  (sin(phasor *  M_PI * 2.0f * (1.0f)  ) * 0.0346f)+
                                 (sin(phasor *  M_PI * 2.0f * (2.0f)  ) * 0.0371f)+
                                 (sin(phasor *  M_PI * 2.0f * (3.0f)  ) * 0.00794f)+
                                 (sin(phasor *  M_PI * 2.0f * (4.0f)  ) * 0.0582f)+
@@ -537,12 +539,10 @@ void loadStops(){
                                 (sin(phasor *  M_PI * 2.0f * (18.0f) ) * 0.00269f)+
                                 (sin(phasor *  M_PI * 2.0f * (19.0f) ) * 0.00231f)+
                                 (sin(phasor *  M_PI * 2.0f * (20.0f) ) * 0.00121f)+
-                                (sin(phasor *  M_PI * 2.0f * (21.0f) ) * 0.000363f)
-                                );
+                                (sin(phasor *  M_PI * 2.0f * (21.0f) ) * 0.000363f);
 
                      //Twelfth 2 2/3
-                  tweTwo[i] = (0.5 *
-                              (sin(phasor *  M_PI * 2.0f * 1.0f   ) * 0.0822f)+
+                  tweTwo[i] = (sin(phasor *  M_PI * 2.0f * 1.0f   ) * 0.0822f)+
                               (sin(phasor *  M_PI * 2.0f * 2.0f   ) * 0.0749f)+
                               (sin(phasor *  M_PI * 2.0f * 3.0f   ) * 0.00841f)+
                               (sin(phasor *  M_PI * 2.0f * 4.0f   ) * 0.00234f)+
@@ -554,8 +554,7 @@ void loadStops(){
                               (sin(phasor *  M_PI * 2.0f * 10.0f  ) * 0.000530f)+
                               (sin(phasor *  M_PI * 2.0f * 11.0f  ) * 0.00146f)+
                               (sin(phasor *  M_PI * 2.0f * 12.0f  ) * 0.000363f)+
-                              (sin(phasor *  M_PI * 2.0f * 13.0f  ) * 0.000305f)
-                              );
+                              (sin(phasor *  M_PI * 2.0f * 13.0f  ) * 0.000305f);
                      //Violone Eight
                   vioEight[i] = (sin(phasor *  M_PI * 2.0f * 1.0f     ) * 0.197f)+
                                 (sin(phasor *  M_PI * 2.0f * 2.0f     ) * 0.141f)+
